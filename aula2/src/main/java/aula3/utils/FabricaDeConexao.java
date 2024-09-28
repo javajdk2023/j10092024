ef5398aa467760d2b7abe0e41abf74c1693f22b0
javajdk2023@gmail.com <javajdk2023@gmail.com>
2024-09-28 13:12:31 -0300

    funcionalidade: carregar credenciais de banco de dados a partir do arquivo application.properties.

--- aula2/src/main/java/aula3/utils/FabricaDeConexao.java
+++ aula2/src/main/java/aula3/utils/FabricaDeConexao.java
@@ -3,6 +3,7 @@ package aula3.utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaDeConexao {
 
@@ -10,14 +11,17 @@ public class FabricaDeConexao {
 	//desafio: 1 método e vários bancos diferentes
 	//desafio 2: singleton - conexao unica
 	public Connection getConnection() {
-
+		
+		//Classe utilitária que carrega os dados de um arquivo
+		Properties props = ReadProperties.loadProperties();
+		
 		// conexao
-		String url = "jdbc:postgresql://localhost:5432/postgres";
-		String usuario = "postgres";
-		String senha = "123";
+		String url = props.getProperty("url");
+		String usuario = props.getProperty("usuario");
+		String senha = props.getProperty("senha");
 
 		try {
-			Connection myConnection = DriverManager.getConnection(url, usuario, senha);
+			Connection myConnection = DriverManager.getConnection(url,usuario, senha);
 			System.out.println("Conectado com sucesso!");
 			return myConnection;
 		} catch (SQLException e) {
